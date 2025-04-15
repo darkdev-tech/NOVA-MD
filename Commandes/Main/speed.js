f47fe7aecb1c4cb9a20fad422d67c1788690c462
Dark hacker 😈 <javieldev@gmail.com>
2025-04-15 11:25:40 +0300

    Update speed.js

--- Commandes/Main/speed.js
+++ Commandes/Main/speed.js
@@ -1,1 +1,19 @@
+// Commandes/main/speed.js
 
+const { cmd } = require("../command");
+const { performance } = require("perf_hooks");
+
+cmd({
+  pattern: "speed",
+  desc: "Check bot response speed",
+  category: "main",
+  react: "⚡",
+  filename: __filename
+},
+async (conn, mek, m, { reply }) => {
+  const start = performance.now();
+  await reply("🕒 Testing speed...");
+  const end = performance.now();
+  const speed = (end - start).toFixed(3);
+  reply(`⚡ *Speed:* _${speed} ms_`);
+});

a5313e8bdbe17f29c4bc5ad7518877a4036c7f01
Dark hacker 😈 <javieldev@gmail.com>
2025-04-15 14:13:19 +0300

    Update and rename Mm to user.js

---
+++ bdd/user.js
@@ -0,0 +1,20 @@
+{
+  "1234567890@s.whatsapp.net": {
+    "name": "Cool Kid",
+    "xp": 200,
+    "level": 3,
+    "banned": false,
+    "warns": 0,
+    "registered": true,
+    "role": "User"
+  },
+  "9876543210@s.whatsapp.net": {
+    "name": "Tester",
+    "xp": 100,
+    "level": 1,
+    "banned": false,
+    "warns": 1,
+    "registered": false,
+    "role": "Guest"
+  }
+}

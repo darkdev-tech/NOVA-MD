31885a75f1f93370d1b2ca569e0bcff5d6ea3de9
Dark hacker 😈 <javieldev@gmail.com>
2025-04-15 14:36:52 +0300

    Update user.js

--- bdd/user.js
+++ bdd/user.js
@@ -1,20 +1,22 @@
 {
   "1234567890@s.whatsapp.net": {
     "name": "Cool Kid",
-    "xp": 200,
+    "exp": 1200,
     "level": 3,
+    "limit": 10,
+    "warn": 1,
+    "premium": false,
     "banned": false,
-    "warns": 0,
-    "registered": true,
-    "role": "User"
+    "registered": true
   },
   "9876543210@s.whatsapp.net": {
-    "name": "Tester",
-    "xp": 100,
-    "level": 1,
+    "name": "NovaDev",
+    "exp": 560,
+    "level": 2,
+    "limit": 15,
+    "warn": 0,
+    "premium": true,
     "banned": false,
-    "warns": 1,
-    "registered": false,
-    "role": "Guest"
+    "registered": true
   }
 }
